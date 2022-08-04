16adfaabd600d65d5adfc477398c05b2fcd81fba
igor.ljubin <igor.ljubin@hrpotentials.com>
2022-08-04 17:45:03 +0200

    languages name to jsonb

--- database/migrations/2022_07_18_132453_create_groups.php
+++ database/migrations/2022_07_18_132453_create_groups.php
@@ -6,72 +6,72 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create(
-            'groups',
-            function (Blueprint $table) {
-                $table->id();
-                $table->string('slug')->unique();
-                $table->string('name');
-                $table->unsignedInteger('group_type_id');
-                $table->foreign('group_type_id')
-                    ->references('id')
-                    ->on('group_types')
-                    ->onUpdate('cascade')
-                    ->onDelete('cascade');
-            }
-        );
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create(
+			'groups',
+			function (Blueprint $table) {
+				$table->id();
+				$table->string('slug')->unique();
+				$table->jsonb('name');
+				$table->unsignedInteger('group_type_id');
+				$table->foreign('group_type_id')
+					->references('id')
+					->on('group_types')
+					->onUpdate('cascade')
+					->onDelete('cascade');
+			}
+		);
 
-        DB::table('groups')->insert(
-            [
-            // COUNTRIES
-            [ 'slug' => 'si', 'name' => '{"sl":"Slovenija", "name" => "hr":"Slovenija", "name" => "it":"Slovenia", "name" => "de":"Slowenien", "name" => "ro":"Slovenia", "name" => "sk":"Slovinsko"}', 'group_type_id' => 1 ],
-            [ 'slug' => 'hr', 'name' => '{"sl":"Hrvaška", "name" => "hr":"Hrvatska", "name" => "it":"Croazia", "name" => "de":"Kroatien", "name" => "ro":"Croaţia", "name" => "sk":"Chorvátsko"}', 'group_type_id' => 1 ],
-            [ 'slug' => 'it', 'name' => '{"sl":"Italija", "name" => "hr":"Italija", "name" => "it":"Italia", "name" => "de":"Italien", "name" => "ro":"Italia", "name" => "sk":"Taliansko"}', 'group_type_id' => 1  ],
-            [ 'slug' => 'at', 'name' => '{"sl":"Avstrija", "name" => "hr":"Austrija", "name" => "it":"Austria", "name" => "de":"Österreich", "name" => "ro":"Austria", "name" => "sk":"Rakúsko"}', 'group_type_id' => 1  ],
-            [ 'slug' => 'ro', 'name' => '{"sl":"Romunia", "name" => "hr":"Rumunjska", "name" => "it":"Romania", "name" => "de":"Rumänien", "name" => "ro":"România", "name" => "sk":"Rumunsko"}', 'group_type_id' => 1  ],
-            [ 'slug' => 'sl', 'name' => '{"sl":"Slovaška", "name" => "hr":"slovački", "name" => "it":"Slovacco", "name" => "de":"slowakisch", "name" => "ro":"Slovacă", "name" => "sk":"Slovenský"}', 'group_type_id' => 1 ],
+		DB::table('groups')->insert(
+			[
+			// COUNTRIES
+			[ 'slug' => 'si', 'name' => '{"sl":"Slovenija", "hr":"Slovenija", "it":"Slovenia", "de":"Slowenien", "ro":"Slovenia", "sk":"Slovinsko"}', 'group_type_id' => 1 ],
+			[ 'slug' => 'hr', 'name' => '{"sl":"Hrvaška", "hr":"Hrvatska", "it":"Croazia", "de":"Kroatien", "ro":"Croaţia", "sk":"Chorvátsko"}', 'group_type_id' => 1 ],
+			[ 'slug' => 'it', 'name' => '{"sl":"Italija", "hr":"Italija", "it":"Italia", "de":"Italien", "ro":"Italia", "sk":"Taliansko"}', 'group_type_id' => 1  ],
+			[ 'slug' => 'at', 'name' => '{"sl":"Avstrija", "hr":"Austrija", "it":"Austria", "de":"Österreich", "ro":"Austria", "sk":"Rakúsko"}', 'group_type_id' => 1  ],
+			[ 'slug' => 'ro', 'name' => '{"sl":"Romunia", "hr":"Rumunjska", "it":"Romania", "de":"Rumänien", "ro":"România", "sk":"Rumunsko"}', 'group_type_id' => 1  ],
+			[ 'slug' => 'sl', 'name' => '{"sl":"Slovaška", "hr":"slovački", "it":"Slovacco", "de":"slowakisch", "ro":"Slovacă", "sk":"Slovenský"}', 'group_type_id' => 1 ],
 
-            // SI-OEGZS
-            [ 'slug' => 'SI-OEGZS-14',  'name' => '{"sl":"SEŽANA"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-10',  'name' => '{"sl":"NAZARJE"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-09',  'name' => '{"sl":"CELJE"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-01',  'name' => '{"sl":"TOLMIN"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-07',  'name' => '{"sl":"NOVO MESTO"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-02',  'name' => '{"sl":"BLED"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-12',  'name' => '{"sl":"MARIBOR"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-05',  'name' => '{"sl":"POSTOJNA"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-03',  'name' => '{"sl":"KRANJ"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-13',  'name' => '{"sl":"MURSKA SOBOTA"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-11',  'name' => '{"sl":"SLOVENJ GRADEC"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-08',  'name' => '{"sl":"BREŽICE"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-04',  'name' => '{"sl":"LJUBLJANA"}', 'group_type_id' => 3 ],
-            [ 'slug' => 'SI-OEGZS-06',  'name' => '{"sl":"KOČEVJE"}', 'group_type_id' => 3 ],
+			// SI-OEGZS
+			[ 'slug' => 'SI-OEGZS-14',  'name' => '{"sl":"SEŽANA"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-10',  'name' => '{"sl":"NAZARJE"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-09',  'name' => '{"sl":"CELJE"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-01',  'name' => '{"sl":"TOLMIN"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-07',  'name' => '{"sl":"NOVO MESTO"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-02',  'name' => '{"sl":"BLED"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-12',  'name' => '{"sl":"MARIBOR"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-05',  'name' => '{"sl":"POSTOJNA"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-03',  'name' => '{"sl":"KRANJ"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-13',  'name' => '{"sl":"MURSKA SOBOTA"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-11',  'name' => '{"sl":"SLOVENJ GRADEC"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-08',  'name' => '{"sl":"BREŽICE"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-04',  'name' => '{"sl":"LJUBLJANA"}', 'group_type_id' => 3 ],
+			[ 'slug' => 'SI-OEGZS-06',  'name' => '{"sl":"KOČEVJE"}', 'group_type_id' => 3 ],
 
-            // PER MODULE ROLES
-            [ 'slug' => 'MBASE2-MODULE-ADMINS',  'name' => '{"sl":"Module admins"}', 'group_type_id' => 2 ],
-            [ 'slug' => 'MBASE2-MODULE-EDITORS',  'name' => '{"sl":"Module data editors"}', 'group_type_id' => 2 ],
-            [ 'slug' => 'MBASE2-MODULE-CONSUMERS',  'name' => '{"sl":"Module data consumers"}', 'group_type_id' => 2 ],
-            [ 'slug' => 'MBASE2-MODULE-READERS',  'name' => '{"sl":"Module data readers"}', 'group_type_id' => 2 ],
-            [ 'slug' => 'MBASE2-MODULE-DEPUTIES',  'name' => '{"sl":"Module deputies"}', 'group_type_id' => 2 ],
-            [ 'slug' => 'MBASE2-MODULE-INTERVENTORS',  'name' => '{"sl":"Module interventors"}', 'group_type_id' => 2 ],
-            ]
-        );
-    }
+			// PER MODULE ROLES
+			[ 'slug' => 'MBASE2-MODULE-ADMINS',  'name' => '{"sl":"Module admins"}', 'group_type_id' => 2 ],
+			[ 'slug' => 'MBASE2-MODULE-EDITORS',  'name' => '{"sl":"Module data editors"}', 'group_type_id' => 2 ],
+			[ 'slug' => 'MBASE2-MODULE-CONSUMERS',  'name' => '{"sl":"Module data consumers"}', 'group_type_id' => 2 ],
+			[ 'slug' => 'MBASE2-MODULE-READERS',  'name' => '{"sl":"Module data readers"}', 'group_type_id' => 2 ],
+			[ 'slug' => 'MBASE2-MODULE-DEPUTIES',  'name' => '{"sl":"Module deputies"}', 'group_type_id' => 2 ],
+			[ 'slug' => 'MBASE2-MODULE-INTERVENTORS',  'name' => '{"sl":"Module interventors"}', 'group_type_id' => 2 ],
+			]
+		);
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('groups');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('groups');
+	}
 };
