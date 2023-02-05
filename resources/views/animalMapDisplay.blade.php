8e1637a503273dc7dd58d6f1b3251ea930f726eb
igor.ljubin <igor.ljubin@hrpotentials.com>
2023-02-05 09:29:05 +0100

    Added leyers to the maps in animal handling and animal list view

--- resources/views/animalMapDisplay.blade.php
+++ resources/views/animalMapDisplay.blade.php
@@ -68,6 +68,27 @@ That should be it.
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);
 
+		baseMaps = {
+			"OpenStreetMap": L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}),
+			"DOF": L.tileLayer.wms("http://prostor4.gov.si:80/ows2-m-pub/wms?", {
+				layers:"SI.GURS.ZPDZ:DOF050",
+				attribution: '<a href="http://www.e-prostor.gov.si/dostop-do-podatkov/dostop-do-podatkov/#c501">Geodetska uprava Republike Slovenije</a>'
+			})
+		};
+
+		overlays = {
+			"Občine":  L.tileLayer.wms("http://prostor4.gov.si:80/ows2-m-pub/wms?", {
+				layers:"table_gurs_pub:SI.GURS.RPE.LS",
+				format: "image/png",
+				transparent: "true",
+				attribution: '<a href="http://www.e-prostor.gov.si/dostop-do-podatkov/dostop-do-podatkov/#c501">Geodetska uprava Republike Slovenije</a>'
+			})
+		};
+
+		L.control.layers(baseMaps, overlays).addTo(map);
+
 		markers.forEach(element => {
 			L.marker([element.position.lat, element.position.lng]).addTo(map)
 			.bindPopup(element.title);
