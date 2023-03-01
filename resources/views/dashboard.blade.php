64c7c27cce84eafc728f7b785ebfaffa6c888629
igor.ljubin <igor.ljubin@hrpotentials.com>
2023-03-01 09:33:11 +0100

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,16 +17,16 @@
 			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 				@auth
 				<div class="p-6 bg-white border-b border-gray-200">
-					<p>Locale is: {{ app()->getLocale() }}</p>
+					<p>{{ __('Locale is:') . ' ' . app()->getLocale() }}</p>
 				</div>
 				<div class="p-6 bg-white border-b border-gray-200">
-					<p>{{__('Data query')}}:</p>
+					<p>{{ __('Data query') }}:</p>
 					<div id="mbaselaravel"></div>
 				</div>
 				@else
 				<div class="p-6 bg-white border-b border-gray-200">
 					<p>{{ __('Please login or register.') }}</p>
-					<p>Locale is: {{ app()->getLocale() }}</p>
+					<p>{{ __('Locale is:') . ' ' .  app()->getLocale() }}</p>
 				</div>
 				@endauth
 			</div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -143,11 +143,11 @@
 					</x-slot>
 				</x-dropdown>
 				@else
-				<a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+				<a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Log in') }}</a>
 
 				@if (Route::has('register'))
 				<a href="{{ route('register') }}"
-					class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+					class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Register') }}</a>
 				@endif
 				@endauth
 			</div>
