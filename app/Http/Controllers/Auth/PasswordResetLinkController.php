c3f2b6e39e164fad4130d0a6b901c558ed300192
Rok Andrée <rok@andree.si>
2023-03-02 16:03:58 +0100

    Adds username support to passowrd reset link

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,45 +3,57 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
 class PasswordResetLinkController extends Controller
 {
-    /**
-     * Display the password reset link request view.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
-    {
-        return view('auth.forgot-password');
-    }
+	/**
+	 * Display the password reset link request view.
+	 *
+	 * @return \Illuminate\View\View
+	 */
+	public function create()
+	{
+		return view('auth.forgot-password');
+	}
 
-    /**
-     * Handle an incoming password reset link request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'email' => ['required', 'email'],
-        ]);
+	/**
+	 * Handle an incoming password reset link request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\RedirectResponse
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function store(Request $request)
+	{
+		$request->validate([
+			'email' => ['required', 'string'],
+		]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+		$email = $request->input('email');
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
-    }
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$user = User::where('username', $email)->first();
+			if ($user) {
+				$email = $user->email;
+			}
+		}
+
+
+
+		// We will send the password reset link to this user. Once we have attempted
+		// to send the link, we will examine the response then see the message we
+		// need to show to the user. Finally, we'll send out a proper response.
+		$status = Password::sendResetLink(
+			['email' => $email]
+		);
+
+		return $status == Password::RESET_LINK_SENT
+			? back()->with('status', __($status))
+			: back()->withInput($request->only('email'))
+			->withErrors(['email' => __($status)]);
+	}
 }
