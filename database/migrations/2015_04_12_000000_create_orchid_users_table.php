68d0a52c22ffbc580fdca6beb1ce256c2aee87e3
igor.ljubin <igor.ljubin@hrpotentials.com>
2023-03-29 02:23:23 +0200

    fixed permissions for admin, fixed error message on password request

--- database/migrations/2015_04_12_000000_create_orchid_users_table.php
+++ database/migrations/2015_04_12_000000_create_orchid_users_table.php
@@ -6,16 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateOrchidUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-    {
-        Schema::table(
-            'users', function (Blueprint $table) {
-                $table->jsonb('permissions')->nullable();
-            }
-        );
+	/**
+	 * Run the migrations.
+	 */
+	public function up()
+	{
+		Schema::table(
+			'users',
+			function (Blueprint $table) {
+				$table->jsonb('permissions')->nullable();
+			}
+		);
 
 		DB::table('users')->insert([
 			'name' => 'admin',
@@ -23,20 +24,20 @@ class CreateOrchidUsersTable extends Migration
 			'email' => 'admin@admin.com',
 			'password' => '$2y$10$OQmT6mhE2UoBx7Mn2e6rIeiqU1ySqXuHxmThEiT9zA7cqnGDn.czS',
 			'created_at' => '2022-07-21 16:52:26',
-			'updated_at' => '2022-07-21 16:52:26',
-			'permissions' => '{"mbase2l.super_admin": true, "mbase2l.admin": true, "platform.index": true, "platform.systems.roles": true, "platform.systems.users": true, "platform.systems.attachment": true}'
+			'updated_at' => '2022-07-21 16:52:26'
 		]);
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
-    {
-        Schema::table(
-            'users', function (Blueprint $table) {
-                $table->dropColumn(['permissions']);
-            }
-        );
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down()
+	{
+		Schema::table(
+			'users',
+			function (Blueprint $table) {
+				$table->dropColumn(['permissions']);
+			}
+		);
+	}
 }
