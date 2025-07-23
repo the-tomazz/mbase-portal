9cb34b16ece07be8770225e40c8f87ec822be976
tomazz <tomazz>
2025-07-23 10:48:48 +0200

    Revert "language icon"
    This reverts commit 155b896cc5164e15f1e70c6a39ac5c32210c5373.

--- app/Casts/LocalizedJsonData.php
+++ app/Casts/LocalizedJsonData.php
@@ -19,10 +19,61 @@ class LocalizedJsonData implements CastsAttributes
      */
     public function get($model, $key, $value, $attributes)
     {
-        return $value;
+        $language = App::getLocale();
+
+        if (is_string($value) && $value !== 'null') {
+            $rawValue = $value;
+            $value = json_decode($value, true);
+
+            if ($value == null) {
+                \Log::error('Invalid json in json encoded database data!', ['lang' => $language, 'data' => $rawValue]);
+                throw new \Exception("Invalid json in json encoded database data!");
+            }
+        }
+
+        // Main feature
+        if (isset($value[$language]) && !empty($value[$language])) {
+            return $value[$language];
+        }
+
+        // Primary fallback - english
+        if (isset($value['en']) && !empty($value['en'])) {
+            return $value['en'];
+        }
+
+        // Anything will do
+        if (is_array($value) && count($value) >= 1) {
+            foreach ($value as $item) {
+                if (!empty($item)) {
+                    return $item;
+                }
+            }
+        }
+
+        // Give up
+        return "";
     }
 
-    public function set($model, $key, $value, $attributes){
-    
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @param  string                              $key
+     * @param  array                               $value
+     * @param  array                               $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        if (is_array($value)) {
+            return json_encode($value);
+        }
+
+		if (isset($attributes[$key])) {
+			$new = json_decode($attributes[$key], true);
+		}
+
+		$new[App::getLocale()] = $value;
+        return json_encode($new);
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -107,7 +107,6 @@
 						<x-slot name="trigger">
 							<button
 								class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-								<x-orchid-icon path="globe" class="w-5 h-5 mr-2 text-gray-400"/>
 								<div>{{ __('common.changeLanguage') }}</div>
 
 								<div class="ml-1">
